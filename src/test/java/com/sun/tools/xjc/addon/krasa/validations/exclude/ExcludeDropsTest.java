2eef4ece0ae826675ffd10aa533aea7f0e863255
Francesco Illuminati <fillumina@gmail.com>
2026-09-21 12:14:22 +0200

    test: the exclude fixtures run as JUnit4
    Twelve fixture tests of the exclude option move from the JUnit3 harness to the JUnit4 base: each
    takes the annotation library as its test parameter, passes it to super, and its arguments stay
    where they were. Not one assertion, expectation file or option string changed, and every class
    still reports two tests, one per library.
    
    Verified: 252/0/0 in the default order, under -Dsurefire.runOrder=alphabetical and
    reversealphabetical, and on JDK 8, same fingerprint in all four (ccbb462af75b over 91 classes);
    javac -Xlint:all and the JDT compiler silent.

--- src/test/java/com/sun/tools/xjc/addon/krasa/validations/exclude/ExcludeDropsTest.java
+++ src/test/java/com/sun/tools/xjc/addon/krasa/validations/exclude/ExcludeDropsTest.java
@@ -1,7 +1,8 @@
 package com.sun.tools.xjc.addon.krasa.validations.exclude;
 
-import com.sun.tools.xjc.addon.krasa.validations.AnnotationCheckerTestHelper;
+import com.sun.tools.xjc.addon.krasa.validations.AnnotationCheckerFixtureTest;
 import com.sun.tools.xjc.addon.krasa.validations.ArgumentBuilder;
+import com.sun.tools.xjc.addon.krasa.validations.ValidationsAnnotation;
 import com.sun.tools.xjc.addon.krasa.validations.ValidationsArgument;
 import java.util.List;
 
@@ -10,14 +11,14 @@ import java.util.List;
  *
  * @author Francesco Illuminati
  */
-public class ExcludeDropsTest extends AnnotationCheckerTestHelper {
+public class ExcludeDropsTest extends AnnotationCheckerFixtureTest {
 
-    public ExcludeDropsTest() {
-        super("exclude", "a", "RootType,ChildType");
+    public ExcludeDropsTest(ValidationsAnnotation library) {
+        super(library, "exclude", "a", "RootType,ChildType");
     }
 
     @Override
-    public List<String> getArgs() {
+    protected List<String> getArgs() {
         return ArgumentBuilder.builder()
                 .add(ValidationsArgument.validationAnnotations, getAnnotation().name())
                 .add("-XJsr303Annotations:exclude=*#code")
--- src/test/java/com/sun/tools/xjc/addon/krasa/validations/exclude/ExcludeTargetsOneAnnotationTest.java
+++ src/test/java/com/sun/tools/xjc/addon/krasa/validations/exclude/ExcludeTargetsOneAnnotationTest.java
@@ -1,7 +1,8 @@
 package com.sun.tools.xjc.addon.krasa.validations.exclude;
 
-import com.sun.tools.xjc.addon.krasa.validations.AnnotationCheckerTestHelper;
+import com.sun.tools.xjc.addon.krasa.validations.AnnotationCheckerFixtureTest;
 import com.sun.tools.xjc.addon.krasa.validations.ArgumentBuilder;
+import com.sun.tools.xjc.addon.krasa.validations.ValidationsAnnotation;
 import com.sun.tools.xjc.addon.krasa.validations.ValidationsArgument;
 import java.util.List;
 
@@ -10,14 +11,14 @@ import java.util.List;
  *
  * @author Francesco Illuminati
  */
-public class ExcludeTargetsOneAnnotationTest extends AnnotationCheckerTestHelper {
+public class ExcludeTargetsOneAnnotationTest extends AnnotationCheckerFixtureTest {
 
-    public ExcludeTargetsOneAnnotationTest() {
-        super("exclude", "a", "RootType,ChildType");
+    public ExcludeTargetsOneAnnotationTest(ValidationsAnnotation library) {
+        super(library, "exclude", "a", "RootType,ChildType");
     }
 
     @Override
-    public List<String> getArgs() {
+    protected List<String> getArgs() {
         return ArgumentBuilder.builder()
                 .add(ValidationsArgument.validationAnnotations, getAnnotation().name())
                 .add("-XJsr303Annotations:exclude=*#label@NotNull")
